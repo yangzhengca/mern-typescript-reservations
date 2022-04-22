ee61c7eac7121e38af849a5a049c22dd580ecd82
yangzhengca <yangzhengca>
2022-04-22 12:07:02 -0600

    added db connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
+require("dotenv").config();
 import express, { Application, Request, Response, NextFunction } from 'express';
-
+const mongoose = require("mongoose");
 const app:Application = express();
+
 const PORT = process.env.PORT || 5001;
+const DB_URL = process.env.DB_URL;
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello TS!")
@@ -10,8 +15,22 @@ app.get('/', (req: Request, res: Response) => {
 
 
 
+// connect to DB then run express server
+mongoose.connect(DB_URL,{ useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=>console.log(`🚀  Server running on port: ${PORT} ...`)))
+    .catch((error:Error)=>console.log(error.message))
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// replaced by the above combined code
+// mongoose.connect(DB_URL, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true
+// });
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//   console.log("Connected to database");
+// });
 
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);  
+// })
